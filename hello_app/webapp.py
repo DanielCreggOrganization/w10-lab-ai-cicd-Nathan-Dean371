450d1cb7cc55658bc45fe21cf78220023cef4817
Nathan Dean <128299047+Nathan-Dean371@users.noreply.github.com>
2025-12-05 15:59:03 +0000

    Update AWS_SECRET_KEY comment in webapp.py and fix assertion in test_intentional_failure for accuracy

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,7 +1,8 @@
 # Entry point for the application.
 from . import app  # noqa: F401
 from . import views  # noqa: F401
-
+# In hello_app/webapp.py
+AWS_SECRET_KEY = "AKIA1234567890"
 # Time-saver: output a URL to the VS Code terminal so you can easily
 # Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
